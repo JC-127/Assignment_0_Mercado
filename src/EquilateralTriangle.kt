7739842acc8861db895e55f5d6f0af50e917333c
Juan-Carlo V. Mercado <tuk35310@temple.edu>
2021-09-05 21:29:09 -0400

    Implementation of main.kt is WIP as well as other class files.

--- src/EquilateralTriangle.kt
+++ src/EquilateralTriangle.kt
@@ -1,5 +1,21 @@
 class EquilateralTriangle (_name : String, varET : String) {
 
     varET = _name
+    val s //this will be the only factor since an equilateral triangle is equal on all sides
+
+    val sc = Scanner(System.'in')
+
+    print("Enter the measurement for the sides of the Equilateral Triangle: ")
+    s = sc.nextDouble()
+
+    fun getArea() {
+
+        AreaEQ =
+
+    }//end getArea()
+
+    fun printDimension() {
+
+    }//end printDimensions()
 
 }//end class EquilateralTriangle
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,46 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+
+    //Will ask the user to enter a shape and use the user input to call a method
+    println("Please enter a shape: ")
+    println("1. Circle\n2. Square\n3. Triangle\n4. Equilateral Triangle")
+
+    val enteredVal = readLine()
+    println("You have entered: $enteredVal")
+
+    val enteredVal2 = Integer.parseInt(enteredVal)
+
+    //TRYING TO IMPLEMENT
+    /*
+    if (enteredVal2 == 1) {
+        Circle()
+    }//end if()
+
+    /*if square is selected, it will call the Equilateral class
+      and prompt the user for the dimensions; calculate the area; print the dimensions
+    */
+    else if (enteredVal2 == 2) {
+        Square()
+    }//end else if()
+
+    /*if Triangle is selected, it will call the Equilateral class
+      and prompt the user for the dimensions; calculate the area; print the dimensions
+    */
+    else if (enteredVal2 == 3) {
+        Triangle()
+    }//end else if()
+
+    /*if Equilateral triangle is selected, it will call the Equilateral class
+      and prompt the user for the dimensions; calculate the area; print the dimensions
+    */
+    else if (enteredVal2 == 4) {
+        EquilateralTriangle()
+    }//end else if()
+
+    //if selection is not a valid selection
+    else {
+        println("Not a valid selection")
+    }//end else()
+*/
+
 }
