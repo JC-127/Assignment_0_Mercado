25939a4f6a831415f3851b97170f40c873688dbe
Juan-Carlo V. Mercado <tuk35310@temple.edu>
2021-09-05 21:29:18 -0400

    Implementation of main.kt is WIP as well as other class files.

--- src/SquareShape.kt
+++ src/SquareShape.kt
@@ -1,6 +1,21 @@
 class SquareShape (_name : String, varS : String) {
 
+    varS = _name
+    val s // side of the square
 
+    val sc = Scanner(System.'in')
 
+    println("Enter the measurement for the side of the Square")
+    s = sc.nextDouble()
+
+    fun getArea() {
+
+        AreaS
+
+    }//end getArea()
+
+    fun printDimensions() {
+
+    }//end printDimensions()
 
 }//end class SquareShape
