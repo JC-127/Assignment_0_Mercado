00985d29c0bc54995742325083fe0251d722e9e9
Juan-Carlo V. Mercado <tuk35310@temple.edu>
2021-09-06 20:17:28 -0400

    Implementation of classes has been created and finished. Only one left needs to be finished is Triangle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,109 @@
+import kotlin.math.sqrt
+
+/***********************************************************************************************************************/
+//SHAPE CLASS
+abstract class Shape () {
+
+    open fun getArea() {}//end getArea()
+
+    open fun printDimensions() {}//end printDimensions
+
+}//end class shape()
+/***********************************************************************************************************************/
+//CIRCLE CLASS
+class Circle : Shape() {
+
+    fun setDimensions() {
+        println("Enter the radius of the circle: ")
+        val radius = readLine()
+        getArea(radius)
+    }//end setDimension()
+
+    //function that will calculate Area of a Circle
+    fun getArea(radius: String?) {
+
+        val pi = 3.14 //STATIC VALUE OF PI; Mmmmm Pie.....
+        val CircleRadius = radius!!.toDouble()
+        val AreaOfCircle = (pi * (CircleRadius * CircleRadius))
+        printDimensions(AreaOfCircle)
+        //println("Area of Circle: $AreaOfCircle")
+    }//end getArea()
+
+    private fun printDimensions(AreaOfCircle: Double) {
+        print("Area of Circle : $AreaOfCircle")
+    }//end printDimensions()
+
+ }//end class Circle : Shape
+/***********************************************************************************************************************/
+//SQUARE CLASS
+class Square : Shape() {
+
+    fun setDimensions() {
+        println("Enter the Length of the Square: ")
+        val sideLength = readLine()
+        println("Enter the Height of the Square: ")
+        val heightLength = readLine()
+        getArea(heightLength, sideLength)
+    }//end setDimension()
+
+    //function that will calculate Area of a Square
+    fun getArea(heightLength: String?, sideLength: String?) {
+
+        val sideOfSquare = sideLength!!.toInt()
+        val AreaOfSquare = (sideOfSquare * sideOfSquare)
+        printDimensions(AreaOfSquare)
+        //print("Area of Square : $AreaOfSquare")
+    }//end getArea()
+
+    private fun printDimensions(AreaOfSquare: Int) {
+        print("Area of Square is: $AreaOfSquare")
+    }//end printDimensions()
+}//end class Square()
+/***********************************************************************************************************************/
+//TRIANGLE CLASS
+class Triangle : Shape() {
+
+    fun setDimension() {
+        println("")
+    }//end setDimension()
+
+    //will calculate area of a triangle using Heron's Formula
+    override fun getArea() {
+
+    }//end getArea()
+
+    /*
+    private fun printDimensions() {
+
+    }
+
+     */
+}//end class Triangle
+/***********************************************************************************************************************/
+//CLASS EQUILATERAL TRIANGLE
+class eTriangle : Shape() {
+
+    fun setDimensions() {
+        println("Enter the length for the Equilateral Triangle (NOTE: ALL SIDES ARE EQUAL IN AN EQUILATERAL TRIANGLE: ")
+        val ETLength = readLine()
+        getArea(ETLength)
+    }//end setDimenstions()
+
+    fun getArea(ETLength: String?) {
+
+        val ETSide = ETLength!!.toDouble()
+        val a = ETSide
+        val AreaOfET = (((a*a)*sqrt(3.0))/4) //SIMPLIFIED HERON'S FORMULA
+        printDimensions(AreaOfET)
+    }//end getArea()
+
+    private fun printDimensions(AreaOfET: Double) {
+        println("Area of Equilateral Triangle: $AreaOfET")
+    }//end printDimensions()
+
+}//end class eTriangle()
+/***********************************************************************************************************************/
+//MAIN FUNCTION
 fun main(args: Array<String>) {
 
     //Will ask the user to enter a shape and use the user input to call a method
@@ -14,6 +120,10 @@ fun main(args: Array<String>) {
     */
     if (enteredVal2 == 1) {
         println("Circle has been chosen")
+        val circle = Circle()
+        circle.setDimensions()
+        circle.getArea()
+        circle.printDimensions()
     }//end if()
 
     /*if square is selected, it will call the Equilateral class
@@ -21,6 +131,10 @@ fun main(args: Array<String>) {
     */
     else if (enteredVal2 == 2) {
         println("Square has been chosen")
+        val square = Square()
+        square.setDimensions()
+        square.getArea()
+        square.printDimensions()
     }//end else if()
 
     /*if Triangle is selected, it will call the Equilateral class
@@ -28,6 +142,10 @@ fun main(args: Array<String>) {
     */
     else if (enteredVal2 == 3) {
         println("Triangle has been chosen")
+        val triangle = Triangle()
+        //triangle.setDimensions()
+        triangle.getArea()
+        //triangle.printDimensions()
     }//end else if()
 
     /*if Equilateral triangle is selected, it will call the Equilateral class
@@ -35,6 +153,10 @@ fun main(args: Array<String>) {
     */
     else if (enteredVal2 == 4) {
         println("Equilateral Triangle has been chosen")
+        val eTriangle = eTriangle()
+        eTriangle.setDimensions()
+        eTriangle.getArea()
+        eTriangle.printDimensions()
     }//end else if()
 
     //if selection is not a valid selection
